41c7b81d2b3dcce12afa0fc7f55f31bca348369c
Tet <ic3dplasma@gmail.com>
2021-04-17 15:45:38 -0700

    why do i keep changing the wrong files

--- hwbounty-home/src/pages/signup.js
+++ hwbounty-home/src/pages/signup.js
@@ -24,8 +24,128 @@ const styles = (theme) => ({
 
 class Signup extends Component {
 	constructor() {
-	
+		super();
+		this.state = {
+			email: "",
+			password: "",
+			fName: "",
+			lName: "",
+			username: "",
+			errors: {},
+			open: true
+		};
+
 	}
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.UI.errors) {
+			this.setState({ errors: nextProps.UI.errors });
+		}
+	}
+	handleSubmit = async (event) => {
+		try {
+			let state = Object.assign({}, this.state);
+			if (state.loggingIn) return;
+			state.loggingIn = true;
+			this.setState(state);
+			event.preventDefault();
+			if (document.getElementById("loginFailedVisible"))
+				document.getElementById("loginFailedVisible").id = "loginFailed"
+			// {
+			// 	"email": "jl38768@pausd.us",
+			// 	"firstName": "Teto",
+			// 	"lastName": "theOldOne",
+			// 	"password": "nya",
+			// 	"username": "tety"
+			//   }
+			const userData = {
+				email: this.state.email,
+				firstName: this.state.fName,
+				lastName: this.state.lName,
+				username: this.state.username,
+				password: this.state.password,
+			};
+			const result = await this.props.signupUser(userData, this.props.history);
+			try {
+				if (!result)
+					document.getElementById("loginFailed").id = "loginFailedVisible"
+				else{
+					alert("Your account is now pending confirmation! Please go into your email and click on the verification link!");
+				}
+					
+			} catch (error) {
+
+			}
+			document.getElementById("loginFailedVisible").innerText = "Signup failed!";
+
+		} catch (error) {
+
+		}
+
+	};
+	handleClose = (event) => {
+		this.setState({
+			open: false
+		})
+	}
+	handleChange = async (event) => {
+		this.setState({
+			[event.target.id]: event.target.value,
+		});
+		if (event.target.id === "username") {
+			let res = (await axios.get(`https://api.hwbounty.help/usernameTaken/${event.target.value}`)).data;
+			if (res) {
+				try {
+					document.getElementById("loginFailed").id = "loginFailedVisible"
+				} catch (error) {
+
+				}
+				document.getElementById("loginFailedVisible").innerText = "Invalid Username! Does it exist already?";
+			} else {
+				try {
+					document.getElementById("loginFailedVisible").id = "loginFailed"
+				} catch (error) {
+
+				}
+			}
+
+		}
+		if (event.target.id === "email") {
+			let res = (await axios.get(`https://api.hwbounty.help/emailTaken/${event.target.value}`)).data;
+			if (res) {
+				try {
+					document.getElementById("loginFailed").id = "loginFailedVisible"
+				} catch (error) {
+
+				}
+				document.getElementById("loginFailedVisible").innerText = "This email is already in use!";
+			} else {
+				try {
+					document.getElementById("loginFailedVisible").id = "loginFailed"
+				} catch (error) {
+
+				}
+			}
+		}
+		if (event.target.id.includes("password")) {
+			//Confirmpassword
+			if (document.getElementById("password").value !== document.getElementById("Confirmpassword").value) {
+
+				try {
+					document.getElementById("loginFailed").id = "loginFailedVisible"
+				} catch (error) {
+
+				}
+				document.getElementById("loginFailedVisible").innerText = "The passwords dont match!";
+			} else {
+				try {
+					document.getElementById("loginFailedVisible").id = "loginFailed"
+				} catch (error) {
+
+				}
+			}
+		}
+		// console.log(this.state)
+	};
 	render() {
 		const {
 			classes,
