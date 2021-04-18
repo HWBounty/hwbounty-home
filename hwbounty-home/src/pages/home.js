499c6c3512d24a0662be994f5faf0225613cc861
Tet <ic3dplasma@gmail.com>
2021-04-18 00:26:48 -0700

    lets goooo

--- hwbounty-home/src/pages/home.js
+++ hwbounty-home/src/pages/home.js
@@ -66,15 +66,16 @@ class Home extends Component {
               if (!localStorage.getItem("user"))
               localStorage.setItem("user",JSON.stringify(this.state.user));
               return `Welcome back ${this.state.user.firstName} 👋!`;
-            }
-            axios.get("https://api.hwbounty.help/@me").then(res=>{
-              if (res.status == 200 && res.data && res.data.password){
-                this.setState({user:res.data});
-              } 
-            })
+            }else{
+              axios.get("https://api.hwbounty.help/@me").then(res=>{
+                if (res.status == 200 && res.data && res.data.password){
+                  this.setState({user:res.data});
+                } 
+              });
+            };
             if (localStorage.getItem("user")){
               return `Welcome back ${JSON.parse(localStorage.getItem("user")).firstName} 👋!`;
-            }
+            };
           })()
           }
         </Typography>
--- hwbounty-home/src/util/randomBackground.js
+++ hwbounty-home/src/util/randomBackground.js
@@ -6,7 +6,7 @@ export const getRandomBackground = ()=>{
 		"https://images7.alphacoders.com/108/1080593.png",
 		"https://images5.alphacoders.com/737/737385.jpg",
 		"https://images3.alphacoders.com/764/764967.png",
-		"https://images3.alphacoders.com/776/776494.png",
+		"https://images3.alphacoders.com/776/776494.png"
 	]
-	return randomBgs[Math.floor(Math.random()*randomBgs.length)]
+	return randomBgs[Math.floor(Date.now()/(1000*60)*randomBgs.length)]
 }
