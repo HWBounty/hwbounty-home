74583f5fb10885edb17dceec7cd2d50476870414
Tet <ic3dplasma@gmail.com>
2021-04-21 00:45:29 -0700

    added some music capabilities. Serious redesign needed

---
+++ hwbounty-home/src/pages/viewProfile.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class ProfilePage extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			userData: null,
+			self: false,
+		}
+	}
+	async getUserData(){
+		
+	}
+	render(){
+		if (!this.userData)
+	}
+}
