6567c362cfbc5b4549a9da6e24b1e5c394b63855
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2021-05-08 13:58:56 -0700

    idkk

--- hwbounty-home/src/pages/modules.js
+++ hwbounty-home/src/pages/modules.js
@@ -1,17 +1,29 @@
 // React
 
 import { Paper } from "@material-ui/core";
-import { Calculator } from "../components/Modules/Calculator/Calculator";
+import Calculator from "../components/Modules/Calculator/Calculator";
 
 // MUI Stuff
+import withStyles from "@material-ui/core/styles/withStyles";
+
+const styles = (theme) => ({
+  ...theme.spreadIt,
+  root: {
+    marginLeft: 300, // temporary until john fixes his stuff
+    marginRight: 30,
+    paddingLeft: 50,
+    paddingRight: 50,
+    paddingTop: 30,
+  },
+});
 
 const Modules = (props) => {
+  const { classes } = props;
   return (
-    <Paper>
-      <div>hello sir</div>
+    <Paper className={classes.root}>
       <Calculator />
     </Paper>
   );
 };
 
-export default Modules;
+export default withStyles(styles)(Modules);
