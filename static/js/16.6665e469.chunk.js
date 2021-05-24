1db5dbd44c25c8acdd99db6716550d99bdbde3f2
John Li (Tet) <tet@tet.moe>
2021-05-24 01:33:22 -0700

    Updates

---
+++ static/js/16.6665e469.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonphwbounty-home"]=this["webpackJsonphwbounty-home"]||[]).push([[16],{1523:function(n,t,e){"use strict";e.r(t),e.d(t,"LandingPage",(function(){return a}));var o=e(993),i=(e(0),e(52)),r=e(51),u=e(5),a=function(n){var t=Object(r.f)();return Object(u.jsx)(o.a,{onClick:function(){return n="/dashboard",void t.push(n);var n},children:"It looks like you are not signed in! We are currently building our landing page, but if you want to go to the dashboard, click me!"})};t.default=Object(i.b)()(a)}}]);
+//# sourceMappingURL=16.6665e469.chunk.js.map
