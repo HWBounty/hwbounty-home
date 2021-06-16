7b837681df5a095be7dd195c5c15f30bb31cc8ae
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2021-06-16 14:16:41 -0700

    Updates

---
+++ static/js/19.1b5107f2.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonphwbounty-home"]=this["webpackJsonphwbounty-home"]||[]).push([[19],{1237:function(n,t,e){"use strict";e.r(t),e.d(t,"LandingPage",(function(){return a}));var o=e(954),i=(e(0),e(46)),r=e(47),u=e(6),a=function(n){var t=Object(r.f)();return Object(u.jsx)(o.a,{onClick:function(){return n="/dashboard",void t.push(n);var n},children:"It looks like you are not signed in! We are currently building our landing page, but if you want to go to the dashboard, click me!"})};t.default=Object(i.b)()(a)}}]);
+//# sourceMappingURL=19.1b5107f2.chunk.js.map
