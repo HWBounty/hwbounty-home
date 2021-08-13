2d244a7283fe5c63eba9157efd507e378fc52352
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2021-08-12 21:53:09 -0700

    merge

--- hwbounty-home/src/pages/modulesPicker.js
+++ hwbounty-home/src/pages/modulesPicker.js
@@ -1,5 +1,6 @@
 // React
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 // MUI
 import Paper from "@material-ui/core/Paper";
@@ -16,55 +17,69 @@ const modulePreviews = [
     img: "https://www.involve.me/assets/images/blog/how-to-create-a-simple-price-calculator-and-capture-more-leads/calculator-M.png",
     title: "Calculator",
     description: "this is a calculator. cool!",
-    to: "/calculator",
+    to: "/modules/calculator",
     cols: 1,
   },
   {
-    img: "none",
-    title: "Image",
-    description: "description",
-    to: "/something",
+    img: "https://www.laarchaeology.org/wp-content/uploads/2017/11/science.jpg",
+    title: "Science",
+    description: "Science test this does nothing rn",
+    to: "/science",
     cols: 2,
   },
   {
-    img: "none",
-    title: "Image",
-    description: "description",
-    to: "/something",
+    img: "https://contenthub-static.grammarly.com/blog/wp-content/uploads/2021/01/how-to-write-conclusion-for-essay.jpg",
+    title: "Essay Editor",
+    description: "Its like a code editor, but for essays!",
+    to: "/essay-editor",
     cols: 1,
   },
   {
-    img: "none",
-    title: "Image",
-    description: "description",
-    to: "/something",
+    img: "https://i.all3dp.com/cdn-cgi/image/fit=cover,w=360,gravity=0.5x0.5,format=auto/wp-content/uploads/2018/01/26171647/Academo.png",
+    title: "3d Graphing Calculator",
+    description: "Self-explanitory...",
+    to: "/3d-graph-calc",
     cols: 1,
   },
   {
-    img: "none",
-    title: "Image",
-    description: "description",
+    img: "https://achieve.lausd.net//cms/lib/CA01000043/Centricity/Domain/4/announcements%20new%20size_backtoschool.png",
+    title: "Back to school?",
+    description:
+      "Back to school test this does nothing, just like everything else!",
     to: "/something",
     cols: 1,
   },
 ];
 
 const ModulesPicker = (props) => {
+  const [module, setModule] = useState(0);
+
+  const handleModuleClicked = (index) => {
+    setModule(index);
+  };
+
   return (
     <div>
       <div
-        style={{ maxWidth: "90vw", marginLeft: "auto", marginRight: "auto" }}
+        style={{
+          maxWidth: "90vw",
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginTop: 50,
+        }}
       >
-        <Typography variant="h1">{"Select a Module"}</Typography>
+        <Typography variant="h1" color="textPrimary">
+          {"Select a Module"}
+        </Typography>
         <br />
         <Grid container spacing={2}>
           <Grid item xs={12} sm={12} md={5}>
             <GridList style={{ flex: 3 }} cellHeight={160} cols={3}>
               {React.Children.toArray(
-                modulePreviews.map((preview) => (
+                modulePreviews.map((preview, index) => (
                   <GridListTile
                     cols={preview.cols || 1}
-                    onClick={() => console.log("Click")}
+                    onClick={() => handleModuleClicked(index)}
                     style={{ cursor: "pointer" }}
                   >
                     <img src={preview.img} alt={preview.title} />
@@ -79,7 +94,10 @@ const ModulesPicker = (props) => {
           </Grid>
           <Grid item xs={12} sm={12} md={7}>
             <Card style={{ width: "100%" }}>
-              <Typography variant="h2">{"Module Preview"}</Typography>
+              <Typography variant="h2" color="textPrimary">
+                {"Module Preview"}
+              </Typography>
+              <div>{module}</div>
             </Card>
           </Grid>
         </Grid>
